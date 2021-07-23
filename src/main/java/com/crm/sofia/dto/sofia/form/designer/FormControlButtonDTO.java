29780eb8cdac9329a8882658ff928ab6b1a45acf
Helias Karagozidis <helias.karagozidis@eurodyn.com>
2021-07-23 13:25:18 +0300

    Refactoring structure, cacheing

---
+++ src/main/java/com/crm/sofia/dto/sofia/form/designer/FormControlButtonDTO.java
@@ -0,0 +1,22 @@
+package com.crm.sofia.dto.sofia.form.designer;
+
+import com.crm.sofia.dto.common.BaseDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Accessors(chain = true)
+public class FormControlButtonDTO extends BaseDTO {
+    private String code;
+    private String icon;
+    private String description;
+    private String editor;
+    private  Boolean visible;
+    private String cssClass;
+}
