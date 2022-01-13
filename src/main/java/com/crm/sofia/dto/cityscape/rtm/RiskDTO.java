c92e93bb9551a3cdd04c9190c882fea9eb2639c7
heliaskaragozidis <!1Helias!!>
2022-01-13 09:50:36 +0200

    RTM & Swagger implementation

--- src/main/java/com/crm/sofia/dto/cityscape/rtm/RiskDTO.java
+++ src/main/java/com/crm/sofia/dto/cityscape/rtm/RiskDTO.java
@@ -1,5 +1,18 @@
 package com.crm.sofia.dto.cityscape.rtm;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Accessors(chain = true)
 public class RiskDTO {
 
     private String cve_id;
