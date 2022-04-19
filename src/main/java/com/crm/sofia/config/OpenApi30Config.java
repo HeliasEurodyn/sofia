ef284ac151a40200010a7b56a505ded82913b2cc
heliaskaragozidis <!1Helias!!>
2022-04-19 17:34:07 +0300

    Minor update

--- src/main/java/com/crm/sofia/config/OpenApi30Config.java
+++ src/main/java/com/crm/sofia/config/OpenApi30Config.java
@@ -39,34 +39,3 @@ public class OpenApi30Config {
     }
 
 }
-
-
-
-//@Configuration
-//public class OpenApi30Config {
-//
-//
-//
-//    public OpenApi30Config() {
-//    }
-//
-//    @Bean
-//    public OpenAPI customOpenAPI() {
-//        final String securitySchemeName = "bearerAuth";
-//        final String apiTitle = String.format("Rita RTM API");
-//
-//        return new OpenAPI()
-//                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-//                .components(
-//                        new Components()
-//                                .addSecuritySchemes(securitySchemeName,
-//                                        new SecurityScheme()
-//                                                .name(securitySchemeName)
-//                                                .type(SecurityScheme.Type.HTTP)
-//                                                .scheme("bearer")
-//                                                .bearerFormat("JWT")
-//                                )
-//                );
-//               // .info(new Info().title(apiTitle).version("v3"));
-//    }
-//}
