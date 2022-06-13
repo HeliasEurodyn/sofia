ff3076e9605a40a1955f4a2e535fac561e3a00b7
HeliasEurodyn <helias.karagozidis@eurodyn.com>
2022-06-13 16:07:40 +0300

    Settings Implementation

---
+++ src/main/java/com/crm/sofia/controllers/sofia/settings/SettingsController.java
@@ -0,0 +1,56 @@
+package com.crm.sofia.controllers.sofia.settings;
+
+import com.crm.sofia.dto.sofia.settings.SettingsDto;
+import com.crm.sofia.services.sofia.settings.SettingsService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@Validated
+@RequestMapping("/settings")
+public class SettingsController {
+
+    private final SettingsService settingsService;
+
+    public SettingsController(SettingsService settingsService) {
+        this.settingsService = settingsService;
+    }
+
+    @GetMapping
+    public SettingsDto getObjectById() {
+        return this.settingsService.getObject();
+    }
+
+    @PostMapping
+    public void postObject(@RequestBody SettingsDto settingsDto) {
+         this.settingsService.postObject(settingsDto);
+    }
+
+    @PutMapping
+    public void putObject(@RequestBody SettingsDto settingsDto) {
+         this.settingsService.postObject(settingsDto);
+    }
+
+    @GetMapping(value = "login-image")
+    public String getLoginImage() {
+        return this.settingsService.getLoginImage();
+    }
+
+    @GetMapping(value = "sidebar-image")
+    public String getSidebarImage() {
+        return this.settingsService.getSidebarImage();
+    }
+
+    @GetMapping(value = "icon")
+    public String getIcon() {
+        return this.settingsService.getIcon();
+    }
+
+    @GetMapping(value = "name")
+    public String getName() {
+        return this.settingsService.getName();
+    }
+
+}
---
+++ src/main/java/com/crm/sofia/repository/sofia/settings/SettingsRepository.java
@@ -0,0 +1,30 @@
+package com.crm.sofia.repository.sofia.settings;
+
+import com.crm.sofia.model.sofia.settings.Settings;
+import com.crm.sofia.repository.common.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SettingsRepository extends BaseRepository<Settings> {
+    List<Settings> findAll();
+
+    @Query(" SELECT DISTINCT s.sidebarImage FROM Settings s " +
+            " WHERE s.id =:id ")
+    public String getSidebarImage(@Param("id") String id);
+
+    @Query(" SELECT DISTINCT s.loginImage FROM Settings s " +
+            " WHERE s.id =:id ")
+    public String getLoginImage(@Param("id") String id);
+
+    @Query(" SELECT DISTINCT s.icon FROM Settings s " +
+            " WHERE s.id =:id ")
+    public String getIcon(@Param("id") String id);
+
+    @Query(" SELECT DISTINCT s.name FROM Settings s " +
+            " WHERE s.id =:id ")
+    public String getName(@Param("id") String id);
+}
