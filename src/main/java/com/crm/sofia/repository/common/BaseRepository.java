3e8040d9e41561c2474e32faa17f311895c4dbcd
christos.aslamatzidis <christos.aslamatzidis@eurodyn.com>
2022-09-12 09:57:30 +0300

    In BaseRepository ,PageAndSortingRepository has been replaced by JPARepository

--- src/main/java/com/crm/sofia/repository/common/BaseRepository.java
+++ src/main/java/com/crm/sofia/repository/common/BaseRepository.java
@@ -2,11 +2,11 @@ package com.crm.sofia.repository.common;
 
 import com.crm.sofia.model.common.BaseEntity;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @Primary
-public interface BaseRepository<M extends BaseEntity> extends PagingAndSortingRepository<M, String> {
+public interface BaseRepository<M extends BaseEntity> extends JpaRepository<M, String> {
 
 }
