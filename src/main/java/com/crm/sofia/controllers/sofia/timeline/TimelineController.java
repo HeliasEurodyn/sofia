746c62c5d6ec5bae1ecb65d934bff5f77b4b4b83
christos.aslamatzidis <christos.aslamatzidis@eurodyn.com>
2022-09-14 13:37:04 +0300

    Refactor TimelineController and TimelineService

--- src/main/java/com/crm/sofia/controllers/sofia/timeline/TimelineController.java
+++ src/main/java/com/crm/sofia/controllers/sofia/timeline/TimelineController.java
@@ -5,17 +5,14 @@ import com.crm.sofia.services.sofia.timeline.TimelineService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Slf4j
 @RestController
 @Validated
-@RequestMapping("/timeline-designer")
+@RequestMapping("/timeline")
 public class TimelineController {
 
     @Autowired
@@ -24,6 +21,12 @@ public class TimelineController {
     @GetMapping(path = "/data")
     Object getData(@RequestParam("id") String id, @RequestParam Map<String, String> parameters) {
         return timelineService.getData(id, parameters);
+    }
+
+    @PostMapping(path = "/data")
+    String postData(@RequestParam("id") String id, @RequestParam Map<String, String> parameters) {
+        Object response = timelineService.postData(id, parameters);
+        return "{\"response\": \"" + response.toString() + "\"}";
     }
 
 }
--- src/main/java/com/crm/sofia/services/sofia/timeline/TimelineService.java
+++ src/main/java/com/crm/sofia/services/sofia/timeline/TimelineService.java
@@ -5,51 +5,79 @@ import com.crm.sofia.mapper.sofia.timeline.TimelineMapper;
 import com.crm.sofia.model.sofia.timeline.Timeline;
 import com.crm.sofia.repository.sofia.timeline.TimelineRepository;
 import com.crm.sofia.services.sofia.auth.JWTService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.HibernateException;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
+import javax.transaction.Transactional;
 import java.util.Map;
 
 @Service
 public class TimelineService {
 
-    @Autowired
-    private TimelineMapper timelineMapper;
-    @Autowired
-    private TimelineRepository timelineRepository;
-    @Autowired
-    private JWTService jwtService;
+    private final TimelineMapper timelineMapper;
+    private final TimelineRepository timelineRepository;
+    private final JWTService jwtService;
 
-    @Autowired
-    private EntityManager entityManager;
+    private final EntityManager entityManager;
+
+    public TimelineService(TimelineMapper timelineMapper, TimelineRepository timelineRepository, JWTService jwtService, EntityManager entityManager) {
+        this.timelineMapper = timelineMapper;
+        this.timelineRepository = timelineRepository;
+        this.jwtService = jwtService;
+        this.entityManager = entityManager;
+    }
 
     public Object getData(String id, Map<String, String> parameters) {
+
+        Query query =buildQuery(id,parameters);
+        return query.getResultList();
+    }
+
+    @Transactional
+    @Modifying
+    public Object postData(String id, Map<String, String> parameters) {
+        try {
+            Object lastInsertId;
+            Query query =buildQuery(id,parameters);
+            query.executeUpdate();
+            lastInsertId = entityManager.createNativeQuery("SELECT LAST_INSERT_ID()").getSingleResult();
+            return lastInsertId;
+        } catch (HibernateException ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+        }
+    }
+
+    public TimelineDTO getObject(String id) {
+        Timeline model = timelineRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Object does not exist"));
+        return timelineMapper.map(model);
+    }
+
+    private Query buildQuery(String id, Map<String, String> parameters){
+
         TimelineDTO dto = this.getObject(id);
         String queryString = dto.getQuery();
 
         Query query = entityManager.createNativeQuery(queryString);
 
-        if(queryString.contains(":userid")){
+        if( queryString.contains(":userid")){
             query.setParameter("userid",this.jwtService.getUserId());
         }
 
-        for (Map.Entry<String, String> entry : parameters.entrySet()) {
-            if(queryString.contains(":"+entry.getKey())){
-                query.setParameter(entry.getKey(),entry.getValue());
-            }
-        }
+        parameters
+                .entrySet()
+                .stream()
+                .filter(entry->  queryString.contains(":"+entry.getKey()))
+                .forEach(entry ->query.setParameter(entry.getKey(),entry.getValue()));
 
-        return query.getResultList();
+        return query;
+
     }
 
-    public TimelineDTO getObject(String id) {
-        Timeline model = timelineRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Object does not exist"));;
-        return timelineMapper.map(model);
-    }
 
 }
