fb1abde2d84fca41ee8868e05388ce0ab4d59501
christos.aslamatzidis <christos.aslamatzidis@eurodyn.com>
2022-10-13 17:27:48 +0300

    Add Business Unit

---
+++ src/main/java/com/crm/sofia/model/sofia/business_unit/BusinessUnit.java
@@ -0,0 +1,23 @@
+package com.crm.sofia.model.sofia.business_unit;
+
+import com.crm.sofia.model.common.MainEntity;
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Accessors(chain = true)
+@Entity(name = "BusinessUnit")
+@Table(name = "business_unit")
+public class BusinessUnit extends MainEntity {
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+}
