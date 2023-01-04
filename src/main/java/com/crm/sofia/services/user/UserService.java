b98c714b6f461da1461affd3d352bc372edafbdf
HeliasEurodyn <helias.karagozidis@eurodyn.com>
2023-01-04 12:37:30 +0200

    Oauth2 Implementations

--- src/main/java/com/crm/sofia/services/user/UserService.java
+++ src/main/java/com/crm/sofia/services/user/UserService.java
@@ -1,14 +1,15 @@
 package com.crm.sofia.services.user;
 
-import com.crm.sofia.dto.user.*;
-import com.crm.sofia.exception.OAuth2AuthenticationProcessingException;
+import com.crm.sofia.dto.user.ChangePasswordRequest;
+import com.crm.sofia.dto.user.JwtAuthenticationResponse;
+import com.crm.sofia.dto.user.SignUpRequest;
+import com.crm.sofia.dto.user.UserDTO;
 import com.crm.sofia.exception.UserAlreadyExistAuthenticationException;
 import com.crm.sofia.exception.login.ChangePasswordException;
 import com.crm.sofia.exception.login.IncorrectPasswordException;
 import com.crm.sofia.exception.login.UserNotFoundException;
 import com.crm.sofia.mapper.user.UserMapper;
 import com.crm.sofia.model.user.LocalUser;
-import com.crm.sofia.model.user.Role;
 import com.crm.sofia.model.user.User;
 import com.crm.sofia.repository.user.RoleRepository;
 import com.crm.sofia.repository.user.UserRepository;
@@ -31,16 +32,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
 import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
 import org.springframework.web.server.ResponseStatusException;
 
-import javax.transaction.Transactional;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import javax.validation.constraints.NotBlank;
-import java.nio.charset.Charset;
-import java.time.Instant;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Random;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -53,6 +56,9 @@ public class UserService {
     private final RoleRepository roleRepository;
     private final AuthenticationManager authenticationManager;
     private final TokenProvider tokenProvider;
+    private final EntityManager entityManager;
+    private final PlatformTransactionManager transactionManager;
+    private TransactionTemplate transactionTemplate;
 
     public UserService(UserRepository userRepository,
                        UserMapper userMapper,
@@ -61,7 +67,9 @@ public class UserService {
                        MenuFieldService menuFieldService,
                        RoleRepository roleRepository,
                        AuthenticationManager authenticationManager,
-                       TokenProvider tokenProvider) {
+                       TokenProvider tokenProvider,
+                       EntityManager entityManager,
+                       PlatformTransactionManager transactionManager) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
         this.passwordEncoder = passwordEncoder;
@@ -69,12 +77,16 @@ public class UserService {
         this.roleRepository = roleRepository;
         this.authenticationManager = authenticationManager;
         this.tokenProvider = tokenProvider;
+        this.entityManager = entityManager;
+        this.transactionManager = transactionManager;
+
+        transactionTemplate = new TransactionTemplate(transactionManager);
     }
 
     @Transactional
     public ResponseEntity<?> authenticate(@NotBlank String username, @NotBlank String enteredPassword) {
 
-        User user = userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new );
+        User user = userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
 
         if (passwordEncoder.matches(enteredPassword, user.getPassword())) {
             user.setEnabled(true);
@@ -94,9 +106,9 @@ public class UserService {
 
             UserDTO userDTO = this.userMapper.mapUserToDtoWithMenu(localUser.getUser());
             return ResponseEntity.ok(new JwtAuthenticationResponse(jwt, userDTO));
-        }else{
+        } else {
 
-            throw  new IncorrectPasswordException();
+            throw new IncorrectPasswordException();
         }
     }
 
@@ -104,23 +116,23 @@ public class UserService {
     public void updateCurrentLanguage(Long languageId) {
         String userId = this.jwtService.getUserId();
 
-       this.userRepository.updateCurrentLanguage(userId, languageId);
+        this.userRepository.updateCurrentLanguage(userId, languageId);
     }
 
     @Transactional
     public UserDTO changePassword(ChangePasswordRequest optionalChangePasswordRequest) {
 
         ChangePasswordRequest changePasswordRequest = Optional.ofNullable(optionalChangePasswordRequest)
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error!"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error!"));
 
         User currentUser = userRepository.findByUsername(changePasswordRequest.getUsername())
-                .orElseThrow(()-> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error!"));
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error!"));
 
 
-        if(StringUtils.isNotBlank(changePasswordRequest.getPassword()) || StringUtils.isNotBlank(changePasswordRequest.getRepeatPassword())){
+        if (StringUtils.isNotBlank(changePasswordRequest.getPassword()) || StringUtils.isNotBlank(changePasswordRequest.getRepeatPassword())) {
             if (!changePasswordRequest.getPassword().equals(changePasswordRequest.getRepeatPassword())) {
                 throw new ChangePasswordException();
-            }else {
+            } else {
                 currentUser.setEnabled(true);
                 currentUser.setPassword(passwordEncoder.encode(changePasswordRequest.getPassword()));
             }
@@ -145,32 +157,96 @@ public class UserService {
         return userOptional.get();
     }
 
-    @Transactional
-    public User registerNewUser(final SignUpRequest signUpRequest) throws UserAlreadyExistAuthenticationException {
-        User user = buildUser(signUpRequest);
-
-        user.setCreatedOn(Instant.now());
-        user.setModifiedOn(Instant.now());
-        user.setEnabled(true);
-        user = userRepository.save(user);
-        userRepository.initiateUserInfo(user.getId());
+    public User registerNewUser(final SignUpRequest signUpRequest) {
+        List<Object[]> fields = this.getUserFields();
+        String oauth2UserId = this.getDefaultOauth2User();
+    //    User tmpUser = this.buildUser(signUpRequest);
+        String newId = this.saveUser(fields, oauth2UserId, signUpRequest);
+        User user = this.userRepository.findById(newId).orElseThrow(UserNotFoundException::new);
         return user;
     }
 
-    private User buildUser(final SignUpRequest formDTO) {
-        User user = new User();
-        user.setUsername(formDTO.getDisplayName());
-        user.setEmail(formDTO.getEmail());
-        user.setPassword(passwordEncoder.encode(formDTO.getPassword()));
-        final HashSet<Role> roles = new HashSet<Role>();
-      //  roles.add(roleRepository.findFirstByName(Role.ROLE_USER));
-        user.setRolesSet(roles);
-        user.setProvider(formDTO.getSocialProvider().getProviderType());
-
-        user.setEnabled(true);
-        user.setProviderUserId(formDTO.getProviderUserId());
-        return user;
+    public List<Object[]> getUserFields() {
+        Query query = this.entityManager.createNativeQuery("DESCRIBE user");
+        return query.getResultList();
     }
+
+    public String getDefaultOauth2User() {
+        Query query = this.entityManager.createNativeQuery("SELECT `oauth_prototype_user_id` FROM `settings` WHERE id = '1'");
+        List<String> oauth2UserResults = query.getResultList();
+        return oauth2UserResults.get(0);
+    }
+
+    public String saveUser(List<Object[]> fields, String oauth2UserId, SignUpRequest signUpRequest) {
+
+        List<String> queryFields =
+                fields.stream()
+                        .filter(field -> !field[0].toString().equals("id"))
+                        .filter(field -> !field[0].toString().equals("created_by"))
+                        .filter(field -> !field[0].toString().equals("created_on"))
+                        .filter(field -> !field[0].toString().equals("modified_by"))
+                        .filter(field -> !field[0].toString().equals("modified_on"))
+                        .filter(field -> !field[0].toString().equals("email"))
+                        .filter(field -> !field[0].toString().equals("username"))
+                        .filter(field -> !field[0].toString().equals("provider"))
+                        .filter(field -> !field[0].toString().equals("password"))
+                        .map(field -> field[0].toString())
+                        .collect(Collectors.toList());
+
+        String fieldsString = String.join(",", queryFields);
+        String newId = UUID.randomUUID().toString();
+
+        String userQueryString =
+                " INSERT INTO user (id, created_by, created_on, modified_by, modified_on, email, username, provider, password , " + fieldsString + " ) " +
+                        " SELECT '" + newId + "', 'oauth2', CURRENT_TIMESTAMP, 'oauth2', CURRENT_TIMESTAMP," +
+                        " '" + signUpRequest.getEmail() + "', '" + signUpRequest.getDisplayName() + "', '" + signUpRequest.getSocialProvider().getProviderType() +
+                        "', '" + passwordEncoder.encode(signUpRequest.getPassword()) + "', " + fieldsString +
+                        " FROM user " +
+                        " WHERE id = '" + oauth2UserId + "' ";
+
+        String roleQueryString =
+                " INSERT INTO user_role (user_id, role_id) " +
+                        " SELECT '" + newId + "', role_id " +
+                        " FROM user_role " +
+                        " WHERE user_id = '" + oauth2UserId + "'";
+
+        String languageQueryString =
+                " INSERT INTO user_language (user_id, language_id) " +
+                        " SELECT '" + newId + "', language_id " +
+                        " FROM user_language " +
+                        " WHERE user_id = '" + oauth2UserId + "'";
+
+        transactionTemplate.execute(transactionStatus -> {
+            entityManager.createNativeQuery(userQueryString)
+                    .executeUpdate();
+
+            entityManager.createNativeQuery(roleQueryString)
+                    .executeUpdate();
+
+            entityManager.createNativeQuery(languageQueryString)
+                    .executeUpdate();
+            transactionStatus.flush();
+            return null;
+        });
+
+
+
+     //   Query query = this.entityManager.createNativeQuery(queryString);
+     //   query.executeUpdate();
+
+        return newId;
+    }
+
+//    private User buildUser(final SignUpRequest formDTO) {
+//        User user = new User();
+//        user.setUsername(formDTO.getDisplayName());
+//        user.setEmail(formDTO.getEmail());
+//        user.setPassword(passwordEncoder.encode(formDTO.getPassword()));
+//        user.setProvider(formDTO.getSocialProvider().getProviderType());
+//        user.setEnabled(true);
+//        user.setProviderUserId(formDTO.getProviderUserId());
+//        return user;
+//    }
 
     public User findUserByEmail(final String email) {
         return userRepository.findByEmail(email);
@@ -184,24 +260,19 @@ public class UserService {
     public LocalUser processUserRegistration(String registrationId, Map<String, Object> attributes, OidcIdToken idToken, OidcUserInfo userInfo) throws UserAlreadyExistAuthenticationException {
         OAuth2UserInfo oAuth2UserInfo = OAuth2UserInfoFactory.getOAuth2UserInfo(registrationId, attributes);
         SignUpRequest userDetails = toUserRegistrationObject(registrationId, oAuth2UserInfo);
-        //User user = findUserByProviderId(oAuth2UserInfo.getProvider(), oAuth2UserInfo.getId());
         User user = this.findUserByUsername(oAuth2UserInfo.getName());
-//        if (user != null) {
-//            if (!user.getProvider().equals(registrationId) && !user.getProvider().equals(SocialProvider.LOCAL.getProviderType())) {
-//                throw new OAuth2AuthenticationProcessingException(
-//                        "Looks like you're signed up with " + user.getProvider() + " account. Please use your " + user.getProvider() + " account to login.");
-//            }
-//        } else {
+
         if (user == null) {
             user = registerNewUser(userDetails);
         }
+
         return LocalUser.create(user, attributes, idToken, userInfo);
     }
 
     private SignUpRequest toUserRegistrationObject(String registrationId, OAuth2UserInfo oAuth2UserInfo) {
         byte[] array = new byte[10]; // length is bounded by 7
         new Random().nextBytes(array);
-        String passwordString = new String(array, Charset.forName("UTF-8"));
+        String passwordString = new String(array, StandardCharsets.UTF_8);
 
         return SignUpRequest.getBuilder()
                 .addProviderUserID(oAuth2UserInfo.getId())
@@ -216,7 +287,7 @@ public class UserService {
 
     public User findUserByUsername(final String username) {
         Optional<User> optionalUser = userRepository.findByUsername(username);
-        if(optionalUser.isEmpty()){
+        if (optionalUser.isEmpty()) {
             return null;
         } else {
             return optionalUser.get();
