57560125a370f2887c794e39601ab8e6f02884b7
helias <helias.karagozidis@eurodyn.com>
2023-02-20 12:30:43 +0200

    Html template implementations

--- src/main/java/com/crm/sofia/controllers/html_template/HtmlTemplateController.java
+++ src/main/java/com/crm/sofia/controllers/html_template/HtmlTemplateController.java
@@ -1,20 +1,17 @@
 package com.crm.sofia.controllers.html_template;
 
-import com.crm.sofia.dto.component.designer.ComponentDTO;
-import com.crm.sofia.dto.component.user.ComponentUiDTO;
 import com.crm.sofia.dto.html_template.HtmlTemplateDTO;
 import com.crm.sofia.services.html_template.HtmlTemplateService;
 import com.lowagie.text.DocumentException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.Map;
-
 
 
 @Slf4j
@@ -30,24 +27,42 @@ public class HtmlTemplateController {
         return htmlTemplateService.getObject(id);
     }
 
-    @GetMapping(path = "/download2")
-    public void print(@RequestParam("id") String id, HttpServletResponse response) throws IOException, DocumentException {
-        htmlTemplateService.download(id, response);
+//    @GetMapping(path = "/download2")
+//    public void print(@RequestParam("id") String id, HttpServletResponse response) throws IOException, DocumentException {
+//        htmlTemplateService.download(id, response);
+//    }
+//
+//    @GetMapping(path = "/download")
+//    void download(@RequestParam("id") String htmlTemplateId,
+//                       @RequestParam("selection-id") String selectionId,
+//            HttpServletResponse response) throws DocumentException, IOException {
+//         this.htmlTemplateService.print(htmlTemplateId, selectionId, response);
+//    }
+
+    @GetMapping(value = "/instant-access-token")
+    Map getToken(@RequestParam("id") String id,
+                 @RequestParam("selection-id") String selectionId) throws DocumentException, IOException {
+        String token = this.htmlTemplateService.getToken(id, selectionId);
+        return Collections.singletonMap("token", token);
     }
 
-    @GetMapping(path = "/download")
-    void download(@RequestParam("id") String htmlTemplateId,
-                       @RequestParam("selection-id") String selectionId,
-            HttpServletResponse response) throws DocumentException, IOException {
-         this.htmlTemplateService.print(htmlTemplateId, selectionId, response);
-    }
+//    @GetMapping(value = "/preview-page.html", produces = MediaType.TEXT_HTML_VALUE)
+//    @ResponseBody
+//    String preview(@RequestParam("id") String htmlTemplateId,
+//                     @RequestParam("selection-id") String selectionId) throws DocumentException, IOException {
+//        return this.htmlTemplateService.printPreview(htmlTemplateId, selectionId);
+//    }
 
     @GetMapping(path = "/print-preview")
-    Map printPreview(@RequestParam("id") String htmlTemplateId,
-                     @RequestParam("selection-id") String selectionId) throws DocumentException, IOException {
-        String html = this.htmlTemplateService.printPreview(htmlTemplateId, selectionId);
-        return Collections.singletonMap("html",html);
+    Map printPreview(@RequestParam("token") String token) {
+        String html = this.htmlTemplateService.printPreview(token);
+        return Collections.singletonMap("html", html);
     }
 
+    @GetMapping(value = "/preview-page.html", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    String preview(@RequestParam("token") String token) {
+        return this.htmlTemplateService.printPreview(token);
+    }
 
 }
--- src/main/java/com/crm/sofia/repository/html_template/HtmlTemplateRepository.java
+++ src/main/java/com/crm/sofia/repository/html_template/HtmlTemplateRepository.java
@@ -3,8 +3,9 @@ package com.crm.sofia.repository.html_template;
 
 import com.crm.sofia.model.html_template.HtmlTemplate;
 import com.crm.sofia.repository.common.BaseRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface HtmlTemplateRepository extends BaseRepository<HtmlTemplate> {
 
 }
