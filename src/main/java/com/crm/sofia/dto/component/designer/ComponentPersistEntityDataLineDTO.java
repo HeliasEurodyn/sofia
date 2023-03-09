78aff4eb3e109ae714c2f77ecb62987958a65638
helias <helias.karagozidis@eurodyn.com>
2023-03-09 17:45:19 +0200

    HtmlTemplate Multilines SubComponents run on Iterations fix

--- src/main/java/com/crm/sofia/dto/component/designer/ComponentPersistEntityDataLineDTO.java
+++ src/main/java/com/crm/sofia/dto/component/designer/ComponentPersistEntityDataLineDTO.java
@@ -10,6 +10,7 @@ import lombok.experimental.Accessors;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 
 @Data
@@ -22,6 +23,13 @@ public class ComponentPersistEntityDataLineDTO implements Serializable {
     private List<ComponentPersistEntityFieldDTO> componentPersistEntityFieldList;
 
     private List<ComponentPersistEntityDTO> componentPersistEntityList = new ArrayList<>();
+
+    public Stream<ComponentPersistEntityDTO> flatSingleLineCPETree(){
+
+        return this.componentPersistEntityList.stream()
+                        .filter(cpe -> !(cpe.getMultiDataLine() == null ? false : cpe.getMultiDataLine()))
+                        .flatMap(ComponentPersistEntityDTO::flatSingleLineCPETree);
+    }
 
 }
 
