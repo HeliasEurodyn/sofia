eaf0cc5ae2ab9bab188af2612c9330a3a00e80b1
helias <helias.karagozidis@eurodyn.com>
2023-04-19 12:18:49 +0300

    Rules implementations

---
+++ src/main/java/com/crm/sofia/dto/rule/RuleDTO.java
@@ -0,0 +1,35 @@
+package com.crm.sofia.dto.rule;
+
+import com.crm.sofia.dto.common.BaseDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Accessors(chain = true)
+public class RuleDTO extends BaseDTO {
+
+    private String code;
+
+    private String name;
+
+    private String description;
+
+    private List<RuleExpressionDTO> ruleExpressionList;
+
+    public RuleDTO(String id, String name, Instant modifiedOn) {
+        this.setId(id);
+        this.name = name;
+    }
+
+}
---
+++ src/main/java/com/crm/sofia/dto/rule/RuleExpressionDTO.java
@@ -0,0 +1,34 @@
+package com.crm.sofia.dto.rule;
+
+import com.crm.sofia.dto.common.BaseDTO;
+import com.crm.sofia.dto.menu.MenuFieldDTO;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Accessors(chain = true)
+public class RuleExpressionDTO extends BaseDTO {
+
+    String field;
+
+    String operator;
+
+    String command;
+
+    String childrenColor;
+
+    String color;
+
+    String joinType;
+
+    String childrenJoinType;
+
+    private List<RuleExpressionDTO> ruleExpressionList = null;
+}
