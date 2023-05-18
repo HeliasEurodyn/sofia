7e02271afd6c9d31bbb7fc2e43562a47e1a1c482
helias <helias.karagozidis@eurodyn.com>
2023-05-18 15:39:56 +0300

    InfoController created

---
+++ src/main/java/com/crm/sofia/controllers/info/InfoController.java
@@ -0,0 +1,22 @@
+package com.crm.sofia.controllers.info;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@Validated
+@RequestMapping("/info")
+public class InfoController {
+
+    @GetMapping(value = "/version")
+    Map getVersion() {
+        return Collections.singletonMap("version", "1.1");
+    }
+
+}
